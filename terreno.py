b653ed09781f3b8aec8825c959d9f2b52c6185d3
Diego Barbosa <diegobarbox@gmail.com>
2022-01-26 16:20:07 -0300

    Calculando área e preço de um terreno.

---
+++ terreno.py
@@ -0,0 +1,13 @@
+# Fazer um programa para ler as medidas da largura e comprimento de um terreno retangular com uma
+# casa decimal, bem como o valor do metro quadrado do terreno com duas casas decimais. Em seguida,
+# o programa deve mostrar o valor da área do terreno, bem como o valor do preço do terreno, ambos com
+# duas casas decimais.
+
+largura = float(input('Digite a largura do terreno: '))
+comprimento = float(input('Digite o comprimento do terreno: '))
+valor_do_metro_quadrado = float(input('Digite o valor do metro quadrado: '))
+area = largura * comprimento
+preco = area * valor_do_metro_quadrado
+print()
+print(f'Área do terreno: {area: .2f}')
+print(f'Preço do terreno: {preco: .2f}')
