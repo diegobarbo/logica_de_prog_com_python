d9a592ed44b4116456941ca89f9cd78a5eb9248e
Diego Barbosa <diegobarbox@gmail.com>
2022-01-28 09:14:45 -0300

    Soma de valores inteiros.

---
+++ estrutura_sequencial/4. soma.py
@@ -0,0 +1,6 @@
+# Fazer um programa para ler dois valores inteiros X e Y, e depois mostrar na tela o valor da soma destes
+# números.
+
+x = int(input('Digite o valor de x: '))
+y = int(input('Digite o valor de y: '))
+print(f'SOMA: {x + y}')
