03161c94857d91f76bd24bd5521b0aebcaa96c24
Diego Barbosa <diegobarbox@gmail.com>
2022-02-04 18:42:36 -0300

    Calcula áreas de um quadrado, um triângulo e um trapézio.

---
+++ estrutura_sequencial/9. medidas.py
@@ -0,0 +1,15 @@
+# Fazer um programa para ler três medidas A, B e C. Em seguida, calcular e mostrar (imprimir os dados
+# com quatro casas decimais):
+# a) a área do quadrado que tem lado A
+# b) a área do triângulo retângulo que base A e altura B
+# c) a área do trapézio que tem bases A e B, e altura C
+
+a = float(input('Digite a medida A: '))
+b = float(input('Digite a medida B: '))
+c = float(input('Digite a medida C: '))
+quadrado = a ** 2
+triangulo = (a * b) / 2
+trapezio = ((a + b) * c) / 2
+print(f'ÁREA DO QUADRADO: {quadrado:.4f}')
+print(f'ÁREA DO TRIÂNGULO: {triangulo:.4f}')
+print(f'ÁREA DO TRAPÉZIO: {trapezio:.4f}')
